6ee5da1e3c336be808f61abf25ab7769dae1b987
patilsnehaljagannath@gmail.com <patilsnehaljagannath@gmail.com>
2024-12-17 15:02:49 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response)=>{
+	response.send("Node app running")
+})
+
+app.listen(4000, ()=>{
+	console.log("server started on port 4000")
+})
+
